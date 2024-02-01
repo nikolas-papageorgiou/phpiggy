f358a891beaed1120d76c46b9eeafa490875c31f
Papageorgiou Nikolaos <59360423+nikolas-papageorgiou@users.noreply.github.com>
2024-02-01 12:18:19 +0200

    ValidatorSerivce created

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -5,16 +5,23 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Framework\TemplateEngine;
+use App\Services\ValidatorService;
+
 
 class AuthController
 {
 
-    public function __construct(private TemplateEngine $view)
+    public function __construct(private TemplateEngine $view,
+    private ValidatorService $validatorService)
     {
     }
 
     public function registerView()
     {
         echo $this->view->render("register.php");
+    }
+    public function register()
+    {
+        dd($_POST);
     }
 }
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -39,6 +39,10 @@ class App
         $this->router->add('GET', $path, $controller);
     }
 
+    public function post(string $path, array $controller)
+    {
+        $this->router->add('POST', $path, $controller);
+    }
 
     public function  addMiddleware(string $middleware)
     {
